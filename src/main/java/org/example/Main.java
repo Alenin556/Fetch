902a1119575e734c35cb7515431f48f64210632f
a.alenin <a.alenin@astondevs.ru>
2024-12-17 17:58:11 +0300

    REBASE

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,24 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Herld!");
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
+        System.out.println("Hello world!");
+    }
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
