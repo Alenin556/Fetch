7f820ffebd7b64e1c554d40f79e584deb2534f84
a.alenin <a.alenin@astondevs.ru>
2024-12-17 17:34:10 +0300

    commit111

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,13 @@ public class Main {
         System.out.println("Hello;
         System.out.println("Hello;
         System.out.println("Hello;
+        System.out.println("Hello;
+        System.out.println("Hello;
+        System.out.println("Hello;
+        System.out.println("Hello;
+        System.out.println("Hello;
+        System.out.println("Hello;
+        System.out.println("Hello;
+        System.out.println("Hello;
     }
 }
