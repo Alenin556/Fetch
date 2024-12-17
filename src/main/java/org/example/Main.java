7cdb42b8b2ce83082d1341cada83bbc69d4eb70a
a.alenin <a.alenin@astondevs.ru>
2024-12-17 17:08:56 +0300

    commit
    (cherry picked from commit 7f8f93a87c2a378198265b516b24cb3cdb69ab1e)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,29 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Herld!");
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
-        System.out.println("Hello;
+        System.out.println("Hello world!");
     }
 }
